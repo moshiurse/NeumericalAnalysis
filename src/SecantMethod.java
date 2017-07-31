9e4f33ec5b0e7099fb6a9cfa57d722942eeaea49
moshiur <rmoshiur705@gmail.com>
2017-08-01 02:02:34 +0600

    space removed

--- src/SecantMethod.java
+++ src/SecantMethod.java
@@ -1,13 +1,11 @@
 
 public class SecantMethod {
-
     // x^4-x-10
 
    double x0 = 1;
    double x1 = 2;
    double x2;
    int countx = 2;
-
    int counter = 5;
 
     public static void main(String[] args){
@@ -22,9 +20,7 @@ public class SecantMethod {
             sm.x1 = sm.x2;
             sm.countx++;
 
-        }
-
-        
+        }        
     }
 
     public double functionOfX(double x){
@@ -37,6 +33,4 @@ public class SecantMethod {
         double x2 = x1-((functionOfX(x1)*(x1-x0)/functionOfX(x1)-functionOfX(x0)));
         return x2;
     }
-
-
 }
